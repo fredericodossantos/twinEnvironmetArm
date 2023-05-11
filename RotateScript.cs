1da473bf0ab6035714deb866c4dc92f72d287ba5
fredericodossantos <frederico.dos.santos@gmail.com>
2023-05-11 16:32:41 -0300

    changed Rotatey I think

--- RotateScript.cs
+++ RotateScript.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class RotateScript : MonoBehaviour
 {
     public float speed = 10f;
@@ -11,26 +9,8 @@ public class RotateScript : MonoBehaviour
     public float yAngle = 0f;
     public float zAngle = 0f;
 
-    private Vector3 currentRotation;
-    private Vector3 targetRotation;
-
-    private void Start()
-    {
-        currentRotation = transform.rotation.eulerAngles;
-        targetRotation = currentRotation + new Vector3(xAngle, yAngle, zAngle);
-    }
-
     private void Update()
     {
-        currentRotation = transform.rotation.eulerAngles;
-
-        // Calculate the rotation difference between current and target rotations
-        Vector3 rotationDifference = targetRotation - currentRotation;
-        if (rotationDifference.magnitude > 0.01f)
-        {
-            // Rotate by the difference multiplied by speed and deltaTime
-            transform.Rotate(rotationDifference.normalized * speed * Time.deltaTime);
-        }
-        else Debug.Log("Distance is " + Vector3.Distance(currentRotation, targetRotation));
+        transform.Rotate(new Vector3(xAngle, yAngle, zAngle)* speed * Time.deltaTime);
     }
 }
