5ea5d821e5094f592aef53744ef99ddaa9e5e09d
fredericodossantos <frederico.dos.santos@gmail.com>
2023-05-22 16:31:09 -0300

    changed to get direct from the game obj

--- ScriptsTest/ToolMove.cs
+++ ScriptsTest/ToolMove.cs
@@ -4,40 +4,35 @@ using UnityEngine;
 
 public class ToolMove : MonoBehaviour
 {
-    public Linear_Acumulator linear_acumulator; // Drag and drop the game object with the Acumulator script attached to this field in the Inspector
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject objectX;
+    public GameObject objectY;
+    public GameObject objectZ;
 
     // Update is called once per frame
-void Update()
-{
-    // Check if there are at least three objects in the gameObjects array
-    if (linear_acumulator.gameObjects.Length >= 3)
+    void Update()
     {
-        // Get the X, Y, and Z positions from the desired game objects
-        float targetX = linear_acumulator.gameObjects[0].transform.position.x;
-        float targetY = linear_acumulator.gameObjects[1].transform.position.y;
-        float targetZ = linear_acumulator.gameObjects[2].transform.position.z;
+        // Check if the required game objects are assigned
+        if (objectX != null && objectY != null && objectZ != null)
+        {
+            // Get the X, Y, and Z positions from the assigned game objects
+            float targetX = objectX.transform.position.x;
+            float targetY = objectY.transform.position.y;
+            float targetZ = objectZ.transform.position.z;
 
-        // Set the target position of the game object
-        Vector3 targetPosition = new Vector3(targetX, targetY, targetZ);
+            // Set the target position of the game object
+            Vector3 targetPosition = new Vector3(targetX, targetY, targetZ);
 
-        // Output the target position for debugging
-        Debug.Log("Target Position: " + targetPosition);
+            // Output the target position for debugging
+            Debug.Log("Target Position: " + targetPosition);
 
-        transform.position = targetPosition; // Use gameObject.transform to reference the game object's transform
+            transform.position = targetPosition; // Use gameObject.transform to reference the game object's transform
 
-        // Set the move speed of the game object
-        linear_acumulator.moveSpeed = 2f;
+            // Set the move speed of the game object
+            // linear_acumulator.moveSpeed = 2f; // If you still need the moveSpeed, uncomment this line or assign it as a public variable
+        }
+        else
+        {
+            Debug.LogError("One or more required game objects are not assigned.");
+        }
     }
-    else
-    {
-        Debug.LogError("Not enough game objects in the gameObjects array");
-    }
-}
-
 }
