d8f5f7b20981668c2d0cee259b3da57bbefec7c7
fredericodossantos <frederico.dos.santos@gmail.com>
2023-05-23 15:46:49 -0300

    modified GCodeReader class that reads a single line from the G-code file and executes the movement for the corresponding axes

--- ScriptsTest/GCodeReader.cs
+++ ScriptsTest/GCodeReader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
+
 public class GCodeReader : MonoBehaviour
 {
     public Linear_Acumulator linear_acumulator;
@@ -23,45 +24,10 @@ public class GCodeReader : MonoBehaviour
             Debug.Log("Trying to read the file");
             using (StreamReader sr = new StreamReader(gcodeFilePath))
             {
-                string line;
-                while ((line = sr.ReadLine()) != null)
+                string line = sr.ReadLine();
+                if (!string.IsNullOrEmpty(line))
                 {
-                    // Parse the G-code commands here
-                    // Extract movement commands for each axis and update the target positions in the Linear_Acumulator script
-                    // For example:
-                    if (line.StartsWith("G1 X"))
-                    {
-                        float targetX = ExtractFloatValue(line, "X");
-                        Vector3 newPosition = linear_acumulator.positions[1];
-                        newPosition.x = targetX;
-                        linear_acumulator.positions[1] = newPosition;
-                        Debug.Log("Changing in X axis");
-
-                        // Initiate movement on the X axis
-                        StartCoroutine(MoveAxis(linear_acumulator, 1));
-                    }
-                    else if (line.StartsWith("G1 Y"))
-                    {
-                        float targetY = ExtractFloatValue(line, "Y");
-                        Vector3 newPosition = linear_acumulator.positions[0];
-                        newPosition.y = targetY;
-                        linear_acumulator.positions[0] = newPosition;
-                        Debug.Log("Changing in Y axis");
-
-                        // Initiate movement on the Y axis
-                        StartCoroutine(MoveAxis(linear_acumulator, 0));
-                    }
-                    else if (line.StartsWith("G1 Z"))
-                    {
-                        float targetZ = ExtractFloatValue(line, "Z");
-                        Vector3 newPosition = linear_acumulator.positions[2];
-                        newPosition.z = targetZ;
-                        linear_acumulator.positions[2] = newPosition;
-                        Debug.Log("Changing in Z axis");
-
-                        // Initiate movement on the Z axis
-                        StartCoroutine(MoveAxis(linear_acumulator, 2));
-                    }
+                    ParseGCodeLine(line);
                 }
             }
         }
@@ -75,51 +41,46 @@ public class GCodeReader : MonoBehaviour
         }
     }
 
-    // Coroutine to move an axis and wait for its completion
-    private IEnumerator MoveAxis(Linear_Acumulator accumulator, int axisIndex)
+    // Parse a single G-code line and execute the corresponding movement
+    private void ParseGCodeLine(string line)
     {
-        accumulator.isMoving[axisIndex] = true;
-        // Perform the movement logic for the specified axis
-        // ...
+        string[] commands = line.Split(' ');
 
-        // Simulating movement completion by waiting for a few seconds
-        yield return new WaitForSeconds(2f);
-
-        accumulator.isMoving[axisIndex] = false;
-    }
-
-
-    // Coroutine to wait for movement completion of a specific axis
-    private IEnumerator WaitForMovementCompletion(Linear_Acumulator accumulator, int axisIndex)
-    {
-        while (accumulator.isMoving[axisIndex])
+        foreach (string command in commands)
         {
-            yield return null;
+            if (command.StartsWith("X"))
+            {
+                float targetX = ExtractFloatValue(command, "X");
+                Vector3 newPosition = linear_acumulator.positions[0];
+                newPosition.x = targetX;
+                linear_acumulator.positions[0] = newPosition;
+                Debug.Log("Changing in X axis");
+            }
+            else if (command.StartsWith("Y"))
+            {
+                float targetY = ExtractFloatValue(command, "Y");
+                Vector3 newPosition = linear_acumulator.positions[1];
+                newPosition.y = targetY;
+                linear_acumulator.positions[1] = newPosition;
+                Debug.Log("Changing in Y axis");
+            }
+            else if (command.StartsWith("Z"))
+            {
+                float targetZ = ExtractFloatValue(command, "Z");
+                Vector3 newPosition = linear_acumulator.positions[2];
+                newPosition.z = targetZ;
+                linear_acumulator.positions[2] = newPosition;
+                Debug.Log("Changing in Z axis");
+            }
         }
     }
-
-    // Coroutine to wait for all movement coroutines to complete
-    // Coroutine to wait for all movement coroutines to complete
-    private IEnumerator WaitForAllMovementsCompletion(List<IEnumerator> movementCoroutines)
-    {
-        foreach (IEnumerator coroutine in movementCoroutines)
-        {
-            yield return StartCoroutine(coroutine);
-        }
-
-        // All movements completed, proceed to the next line in the G-code or perform any necessary action
-        Debug.Log("All movements completed.");
-        // Proceed to the next line or perform additional actions here
-    }
-
 
     // Helper method to extract float values from G-code commands
-    private float ExtractFloatValue(string line, string parameter)
+    private float ExtractFloatValue(string command, string parameter)
     {
-        int parameterIndex = line.IndexOf(parameter);
+        int parameterIndex = command.IndexOf(parameter);
         int startIndex = parameterIndex + parameter.Length;
-        int endIndex = line.IndexOf(' ', startIndex);
-        string valueString = line.Substring(startIndex, endIndex - startIndex);
+        string valueString = command.Substring(startIndex);
         return float.Parse(valueString);
     }
 }
