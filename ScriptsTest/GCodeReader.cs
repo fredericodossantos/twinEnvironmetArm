a83d14f3fd68c650e839d85898f8a047e2ecf8bd
fredericodossantos <frederico.dos.santos@gmail.com>
2023-05-23 15:28:48 -0300

    I will go back in the project

--- ScriptsTest/GCodeReader.cs
+++ ScriptsTest/GCodeReader.cs
@@ -3,15 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
-
 public class GCodeReader : MonoBehaviour
 {
     public Linear_Acumulator linear_acumulator;
     public string gcodeFilePath; // Path to the G-code file
 
     // Attach this method to the button's OnClick event in the Inspector
-
-
     public void ReadAndParseGCode()
     {
         if (string.IsNullOrEmpty(gcodeFilePath))
@@ -23,7 +20,7 @@ public class GCodeReader : MonoBehaviour
         // Read the G-code file
         try
         {
-            Debug.Log("trying to read the file");
+            Debug.Log("Trying to read the file");
             using (StreamReader sr = new StreamReader(gcodeFilePath))
             {
                 string line;
@@ -38,9 +35,10 @@ public class GCodeReader : MonoBehaviour
                         Vector3 newPosition = linear_acumulator.positions[1];
                         newPosition.x = targetX;
                         linear_acumulator.positions[1] = newPosition;
-                        Debug.Log("changing in X axis");
+                        Debug.Log("Changing in X axis");
 
-                        StartCoroutine(WaitForMovementCompletion(linear_acumulator, 1));
+                        // Initiate movement on the X axis
+                        StartCoroutine(MoveAxis(linear_acumulator, 1));
                     }
                     else if (line.StartsWith("G1 Y"))
                     {
@@ -48,9 +46,10 @@ public class GCodeReader : MonoBehaviour
                         Vector3 newPosition = linear_acumulator.positions[0];
                         newPosition.y = targetY;
                         linear_acumulator.positions[0] = newPosition;
-                        Debug.Log("changing in Y axis");
+                        Debug.Log("Changing in Y axis");
 
-                        StartCoroutine(WaitForMovementCompletion(linear_acumulator, 0));
+                        // Initiate movement on the Y axis
+                        StartCoroutine(MoveAxis(linear_acumulator, 0));
                     }
                     else if (line.StartsWith("G1 Z"))
                     {
@@ -58,9 +57,10 @@ public class GCodeReader : MonoBehaviour
                         Vector3 newPosition = linear_acumulator.positions[2];
                         newPosition.z = targetZ;
                         linear_acumulator.positions[2] = newPosition;
-                        Debug.Log("changing in Z axis");
+                        Debug.Log("Changing in Z axis");
 
-                        StartCoroutine(WaitForMovementCompletion(linear_acumulator, 2));
+                        // Initiate movement on the Z axis
+                        StartCoroutine(MoveAxis(linear_acumulator, 2));
                     }
                 }
             }
@@ -75,18 +75,42 @@ public class GCodeReader : MonoBehaviour
         }
     }
 
-    // Coroutine to wait for movement completion
+    // Coroutine to move an axis and wait for its completion
+    private IEnumerator MoveAxis(Linear_Acumulator accumulator, int axisIndex)
+    {
+        accumulator.isMoving[axisIndex] = true;
+        // Perform the movement logic for the specified axis
+        // ...
+
+        // Simulating movement completion by waiting for a few seconds
+        yield return new WaitForSeconds(2f);
+
+        accumulator.isMoving[axisIndex] = false;
+    }
+
+
+    // Coroutine to wait for movement completion of a specific axis
     private IEnumerator WaitForMovementCompletion(Linear_Acumulator accumulator, int axisIndex)
     {
         while (accumulator.isMoving[axisIndex])
         {
             yield return null;
         }
-
-        // Movement completed, proceed to the next G-code command
     }
 
+    // Coroutine to wait for all movement coroutines to complete
+    // Coroutine to wait for all movement coroutines to complete
+    private IEnumerator WaitForAllMovementsCompletion(List<IEnumerator> movementCoroutines)
+    {
+        foreach (IEnumerator coroutine in movementCoroutines)
+        {
+            yield return StartCoroutine(coroutine);
+        }
 
+        // All movements completed, proceed to the next line in the G-code or perform any necessary action
+        Debug.Log("All movements completed.");
+        // Proceed to the next line or perform additional actions here
+    }
 
 
     // Helper method to extract float values from G-code commands
