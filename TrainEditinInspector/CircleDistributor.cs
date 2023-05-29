a9152d28e7b76cf3fc38151456aade19b3d48e75
fredericodossantos <frederico.dos.santos@gmail.com>
2023-05-29 08:51:22 -0300

    radialDistribution

--- TrainEditinInspector/CircleDistributor.cs
+++ TrainEditinInspector/CircleDistributor.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class CircleDistributor : MonoBehaviour
 {
-    public GameObject spherePrefab;
+    public GameObject prefab;
     public int numberOfObjects = 10;
     public float radius = 5f;
+    public float force = 10f;
+    public float interval = 1f;
 
-    void Start()
+    private float timer = 0f;
+
+    void Update()
     {
-        for (int i = 0; i < numberOfObjects; i++)
+        timer += Time.deltaTime;
+        if (timer >= interval)
         {
-            float angle = i * Mathf.PI * 2 / numberOfObjects;
-            Vector3 newPos = new Vector3(Mathf.Cos(angle) * radius, 0, Mathf.Sin(angle) * radius);
-            Instantiate(spherePrefab, newPos, Quaternion.identity);
+            timer = 0f;
+            for (int i = 0; i < numberOfObjects; i++)
+            {
+                float angle = i * Mathf.PI * 2 / numberOfObjects;
+                Vector3 newPos = new Vector3(Mathf.Cos(angle) * radius, 0, Mathf.Sin(angle) * radius);
+                GameObject sphere = Instantiate(prefab, newPos, Quaternion.identity);
+                Rigidbody rb = sphere.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddForce(newPos.normalized * force, ForceMode.Impulse);
+                }
+            }
         }
     }
 }
