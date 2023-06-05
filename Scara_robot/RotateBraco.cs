3b0827e19230ceee7b2851bf5e6ccf55fbdf11da
fredericodossantos <frederico.dos.santos@gmail.com>
2023-06-05 16:06:53 -0300

    Rotation by button

---
+++ Scara_robot/RotateBraco.cs
@@ -0,0 +1,27 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateBraco : MonoBehaviour
+{
+    public Button rotateButton;
+    public GameObject targetObject;
+
+    private bool isRotating = false;
+
+    private void Start()
+    {
+        rotateButton.onClick.AddListener(RotateObject);
+    }
+
+    private void RotateObject()
+    {
+        if (!isRotating)
+        {
+            isRotating = true;
+            targetObject.transform.Rotate(0, 0, 30f);
+            isRotating = false;
+        }
+    }
+}
+
