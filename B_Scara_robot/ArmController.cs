c6f345d4af7fb9bc15145b06154b6828c724bfdf
fredericodossantos <frederico.dos.santos@gmail.com>
2023-06-14 15:27:24 -0300

    armController changed to move just the forearm

--- B_Scara_robot/ArmController.cs
+++ B_Scara_robot/ArmController.cs
@@ -4,36 +4,21 @@ using UnityEngine;
 
 public class ArmController : MonoBehaviour
 {
-    public GameObject Arm;
     public GameObject Forearm;
-    public float segLength = 80f;
+    public GameObject Hand;
 
     void Update()
     {
-        DragSegment(transform, Arm.transform);
-        DragSegment(Arm.transform, Forearm.transform);
+        DragSegment(Forearm.transform);
     }
 
-    void DragSegment(Transform joint1, Transform joint2)
+    void DragSegment(Transform joint)
     {
-        Vector3 target;
-        if (joint2 != null)
-            target = joint2.position;
-        else
-            target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
-
-        float dx = target.x - joint1.position.x;
-        float dy = target.y - joint1.position.y;
+        Vector3 target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+        float dx = target.x - joint.position.x;
+        float dy = target.y - joint.position.y;
         float angle = Mathf.Atan2(dy, dx);
 
-        joint1.rotation = Quaternion.Euler(0f, 0f, angle * Mathf.Rad2Deg);
-
-        if (joint2 != null)
-        {
-            Vector3 joint2Pos = joint1.position;
-            joint2Pos.x += Mathf.Cos(angle) * segLength;
-            joint2Pos.y += Mathf.Sin(angle) * segLength;
-            joint2.position = joint2Pos;
-        }
+        joint.rotation = Quaternion.Euler(0f, 0f, angle * Mathf.Rad2Deg);
     }
 }
