2dce17f2ab40cd854747ff2a2b79aad4d19c1751
fredericodossantos <frederico.dos.santos@gmail.com>
2023-06-18 22:27:23 -0300

    better tentacle

--- Elo_scripts/Tentacle.cs
+++ Elo_scripts/Tentacle.cs
@@ -18,14 +18,11 @@ public class Tentacle : MonoBehaviour
         // Calculate segment lengths
         for (int i = 0; i < segments.Length; i++)
         {
-            if (i == segments.Length - 1)
-            {
-                segmentLengths[i] = Vector3.Distance(segments[i].position, target.position);
-            }
-            else
-            {
-                segmentLengths[i] = Vector3.Distance(segments[i].position, segments[i + 1].position);
-            }
+            Vector3 segmentPosition = segments[i].position;
+            Vector3 nextPosition = (i == segments.Length - 1) ? target.position : segments[i + 1].position;
+            segmentPosition.y = 0f;
+            nextPosition.y = 0f;
+            segmentLengths[i] = Vector3.Distance(segmentPosition, nextPosition);
             Debug.Log("Segment " + i + " length: " + segmentLengths[i]);
         }
 
@@ -38,10 +35,10 @@ public class Tentacle : MonoBehaviour
 
     private void Update()
     {
-        ReachSegment(0, target.position.x, target.position.y);
+        ReachSegment(0, target.position.x, target.position.z);
         for (int i = 1; i < segments.Length; i++)
         {
-            ReachSegment(i, segments[i - 1].position.x, segments[i - 1].position.y);
+            ReachSegment(i, segments[i - 1].position.x, segments[i - 1].position.z);
         }
 
         for (int j = segments.Length - 1; j >= 1; j--)
@@ -58,22 +55,23 @@ public class Tentacle : MonoBehaviour
     private void PositionSegment(int a, int b)
     {
         Vector3 dir = segments[b].position - segments[a].position;
-        angles[a] = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        dir.y = 0f; // Ignore the Y-axis
+        angles[a] = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
         segments[a].position = segments[b].position - dir.normalized * segmentLengths[a];
     }
 
-    private void ReachSegment(int i, float targetX, float targetY)
+    private void ReachSegment(int i, float targetX, float targetZ)
     {
-        Vector3 dir = new Vector3(targetX - segments[i].position.x, targetY - segments[i].position.y, 0f);
-        angles[i] = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Vector3 dir = new Vector3(targetX - segments[i].position.x, 0f, targetZ - segments[i].position.z);
+        angles[i] = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
         Vector3 targetPos = new Vector3(targetX - Mathf.Cos(angles[i] * Mathf.Deg2Rad) * segmentLengths[i],
-                                         targetY - Mathf.Sin(angles[i] * Mathf.Deg2Rad) * segmentLengths[i],
-                                         0f);
+                                         segments[i].position.y,
+                                         targetZ - Mathf.Sin(angles[i] * Mathf.Deg2Rad) * segmentLengths[i]);
         segments[i].position = targetPos;
     }
 
     private void Segment(Transform segment, float angle, float strokeWeight)
     {
-        segment.rotation = Quaternion.Euler(0f, 0f, angle);
+        segment.rotation = Quaternion.Euler(0f, angle, 0f);
     }
 }
