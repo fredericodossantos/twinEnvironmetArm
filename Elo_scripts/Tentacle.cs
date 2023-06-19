90490766a5f1d8bd2f9217e264c9d2c336790287
fredericodossantos <frederico.dos.santos@gmail.com>
2023-06-19 15:00:51 -0300

    The best version till now

--- Elo_scripts/Tentacle.cs
+++ Elo_scripts/Tentacle.cs
@@ -15,16 +15,16 @@ public class Tentacle : MonoBehaviour
         segmentLengths = new float[segments.Length];
         angles = new float[segments.Length];
 
-    // Calculate segment lengths
-    for (int i = 0; i < segments.Length; i++)
-    {
-        Vector3 segmentPosition = segments[i].position;
-        Vector3 nextPosition = (i == segments.Length - 1) ? target.position : segments[i + 1].position;
-        segmentPosition.y = 0f;
-        nextPosition.y = 0f;
-        segmentLengths[i] = i == segments.Length - 1 ? 1f : Vector3.Distance(segmentPosition, nextPosition); // modify this line
-        Debug.Log("Segment " + i + " length: " + segmentLengths[i]);
-    }
+        // Calculate segment lengths
+        for (int i = 0; i < segments.Length; i++)
+        {
+            Vector3 segmentPosition = segments[i].position;
+            Vector3 nextPosition = (i == segments.Length - 1) ? target.position : segments[i + 1].position;
+            segmentPosition.y = 0f;
+            nextPosition.y = 0f;
+            segmentLengths[i] = i == segments.Length - 1 ? 1f : Vector3.Distance(segmentPosition, nextPosition);
+            Debug.Log("Segment " + i + " length: " + segmentLengths[i]);
+        }
 
         // Set initial angles for each segment to 0
         for (int i = 0; i < segments.Length; i++)
@@ -55,22 +55,23 @@ public class Tentacle : MonoBehaviour
     {
         Vector3 dir = segments[b].position - segments[a].position;
         dir.y = 0f; // Ignore the Y-axis
-        angles[a] = -(Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg);
+        angles[a] = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
 
-        segments[a].position = segments[b].position + dir.normalized * segmentLengths[a];
+        // Calculate the target position for the segment
+        Vector3 targetPosition = segments[b].position - dir.normalized * segmentLengths[a];
+
+        // Move the segment towards the target position gradually using Lerp
+        segments[a].position = Vector3.Lerp(segments[a].position, targetPosition, Time.deltaTime * 5f);
     }
 
     private void ReachSegment(int i, float targetX, float targetZ)
     {
         Vector3 dir = new Vector3(targetX - segments[i].position.x, 0f, -(targetZ - segments[i].position.z));
-        angles[i] = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg; // Add 180 degrees to invert rotation
+        angles[i] = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
     }
 
     private void Segment(Transform segment, float angle)
     {
         segment.rotation = Quaternion.Euler(0f, angle, 0f);
     }
-
-
-
 }
