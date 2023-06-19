cb70fb308e12a7c6f46b4cb569c4336dd5670527
fredericodossantos <frederico.dos.santos@gmail.com>
2023-06-18 22:31:22 -0300

    another approach trying to fix the parentest segment

--- Elo_scripts/Tentacle.cs
+++ Elo_scripts/Tentacle.cs
@@ -35,19 +35,17 @@ public class Tentacle : MonoBehaviour
 
     private void Update()
     {
-        ReachSegment(1, target.position.x, target.position.z); // Start from index 1 instead of 0
-
-        for (int i = 2; i < segments.Length; i++) // Start from index 2
+        for (int i = 1; i < segments.Length; i++)
         {
             ReachSegment(i, segments[i - 1].position.x, segments[i - 1].position.z);
         }
 
-        for (int j = segments.Length - 1; j >= 2; j--) // Start from index 2
+        for (int j = segments.Length - 1; j >= 1; j--)
         {
             PositionSegment(j, j - 1);
         }
 
-        for (int k = 1; k < segments.Length; k++) // Start from index 1 instead of 0
+        for (int k = 0; k < segments.Length; k++)
         {
             Segment(segments[k], angles[k], 10f);
         }
@@ -58,17 +56,12 @@ public class Tentacle : MonoBehaviour
         Vector3 dir = segments[b].position - segments[a].position;
         dir.y = 0f; // Ignore the Y-axis
         angles[a] = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
-        segments[a].position = segments[b].position - dir.normalized * segmentLengths[a];
     }
 
     private void ReachSegment(int i, float targetX, float targetZ)
     {
         Vector3 dir = new Vector3(targetX - segments[i].position.x, 0f, targetZ - segments[i].position.z);
         angles[i] = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
-        Vector3 targetPos = new Vector3(targetX - Mathf.Cos(angles[i] * Mathf.Deg2Rad) * segmentLengths[i],
-                                         segments[i].position.y,
-                                         targetZ - Mathf.Sin(angles[i] * Mathf.Deg2Rad) * segmentLengths[i]);
-        segments[i].position = targetPos;
     }
 
     private void Segment(Transform segment, float angle, float strokeWeight)
